6fb214c1b88c02b70a8223b9d41ee6584663eaec
Piotr Tabor <piotr.tabor@gmail.com>
2025-06-22 17:32:07 +0200

    More comments about deploying cloud functions.

--- editor-github/deploy.sh
+++ editor-github/deploy.sh
@@ -18,5 +18,15 @@ gcloud functions deploy songbook-ghe \
   --env-vars-file=google-cloud-functions-env.txt \
   --set-secrets=OAUTH_APP_SECRET=projects/${GOOGLE_PROJECT}/secrets/github-editor-app-oauth:latest
 
+# Domain mapping: https://console.cloud.google.com/run/domains?inv=1&invt=Ab0zqQ&project=spiewaj-com
+gcloud run domain-mappings create --service songbook-ghe \
+  --domain spiewaj.com \
+  --project=${GOOGLE_PROJECT} \
+  --region=europe-west1
+
+# Create a new secret:
+# gcloud secrets create github-editor-app-oauth --project=${GOOGLE_PROJECT} --replication-policy=automatic
+# gcloud secrets versions add github-editor-app-oauth --project=${GOOGLE_PROJECT} ...
 # See secret details: https://console.cloud.google.com/security/secret-manager/secret/github-editor-app-oauth/versions?project=wdw-21
 # Generate new secret: https://github.com/settings/applications/2019824
+# Secreat is stored at the level of github organization (developer settings, oath apps).
