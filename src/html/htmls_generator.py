5db167dbc3b27314624c339a25f8879a1ef14ee0
Piotr Tabor <piotr.tabor@gmail.com>
2025-07-06 00:41:12 +0200

    Ability to edit song streight from the song page.

--- src/html/htmls_generator.py
+++ src/html/htmls_generator.py
@@ -4,6 +4,20 @@ import shutil
 import src.html.create_songs_html as cash
 import sys
 import src.lib.songbook as sb
+from lxml import etree
+
+def load_template(template_file_name):
+    """Load an XHTML template from the given path."""
+    path_template = os.path.join(sb.repo_dir(), 'src', 'html', 'templates', template_file_name)
+    if not os.path.exists(path_template):
+        print(f"Template file not found: {path_template}")
+        sys.exit(1)
+    # file_content = ""
+    # with open(path_template, 'r') as f:
+    #   file_content = f.read()
+    
+    root = etree.parse(path_template, etree.XMLParser(recover=True, remove_blank_text=True)).getroot()
+    return root.getchildren() if root.tag == 'root' else list(root)
 
 def main():
     target_dir = os.path.join(sb.repo_dir(), "build")  # gdzie ma utworzyć epub
@@ -15,7 +29,12 @@ def main():
         shutil.rmtree(path_songs_html)
         os.mkdir(path_songs_html)
 
-    cash.create_all_songs_html(songbook.list_of_songs(), path_songs_html)
+    # load snipped of xhtml using lxml from templates/_song_prefix.xhtml
+    _song_prefix = load_template('_song_prefix.xhtml',)
+    _song_head = load_template('_song_head.xhtml')
+    _song_suffix = load_template('_song_suffix.xhtml')
+    
+    cash.create_all_songs_html(songbook.list_of_songs(), path_songs_html, song_prefix=_song_prefix, song_suffix=_song_suffix, song_head=_song_head)
     path_css = os.path.join(path_songs_html, "CSS")
     os.mkdir(path_css)
 
@@ -28,6 +47,10 @@ def main():
     for css_file in CSS_FILES:
         path_tmp_css = os.path.join(sb.repo_dir(), 'src', 'html', 'css', css_file)
         os.symlink(path_tmp_css, os.path.join(path_css, css_file))
+    if not os.path.exists(os.path.join(target_dir, "common.js")):
+        os.symlink(
+            os.path.join(sb.repo_dir(), 'src', 'html', 'templates', 'common.js'),
+            os.path.join(target_dir, 'common.js'))
 
 if __name__ == "__main__":
     main()
