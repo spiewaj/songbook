9433b25aa97c614b3f15e0e1114bab8a96729d47
Piotr Tabor <piotr.tabor@gmail.com>
2025-07-12 11:27:20 +0200

    Support deploying javascript code.

--- _site/index.js
+++ _site/index.js
@@ -1,16 +1,3 @@
-function edit(file) {
-    let d = document.createElement('form');
-    d.setAttribute("action", "https://ghe.spiewaj.com/users/me/changes:new")
-    d.setAttribute("method", "post")
-    let i = document.createElement("input")
-    i.setAttribute("type", "hidden")
-    i.setAttribute("name", "file")
-    i.setAttribute("value", file)
-    d.appendChild(i);
-
-    document.body.appendChild(d);
-    d.submit();
-}
 function filterSongs() {
     // 1. Get references to the input and the list
     const input = document.getElementById('songSearch');
