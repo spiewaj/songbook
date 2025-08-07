77017879496699e90afb116bb21d255f4e7e1885
Piotr Tabor <piotr.tabor@gmail.com>
2025-08-07 21:37:23 +0200

    3 fixes: search without polish letters. Edit icon works. (#148)

--- _site/index.js
+++ _site/index.js
@@ -1,3 +1,13 @@
+function simplifyString(str) {
+    if (typeof str !== 'string') {
+        return '';
+    }
+    return str
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/ł/gi, 'l'); // Added to handle 'ł' and 'Ł'
+}
+
 function filterSongs() {
     // 1. Get references to the input and the list
     const input = document.getElementById('songSearch');
@@ -11,15 +21,16 @@ function filterSongs() {
         // 3. Find the link (a) inside the list item
         const a = li[i].getElementsByTagName("a")[0];
         if (a) {
-            // 4. Get the text content of the link
             txtValue += ' ' + a.textContent || a.innerText;
         }
         for (const span of li[i].getElementsByTagName("span")) {
-            // 4. Get the text content of the span
-            txtValue += ' ' + span.textContent || span.innerText;
+            if (span.textContent != 'edit') {
+              txtValue += ' ' + span.textContent || span.innerText;
+            }
         }
-        // 5. Check if the song title includes the filter text
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        console.log("Checking: " + simplifyString(txtValue).toUpperCase())
+        if (txtValue.toUpperCase().indexOf(filter) > -1 ||
+            simplifyString(txtValue).toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = ""; // Show the list item
         } else {
             li[i].style.display = "none"; // Hide the list item
